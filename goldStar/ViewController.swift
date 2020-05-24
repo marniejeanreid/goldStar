ed3407b559c5b5c0d4d2b579f22b1f24ff4309f1
Marnie Reid <marniejeanreid@gmail.com>
2020-05-24 13:42:01 -0600

    Initial Commit

---
+++ goldStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  goldStar
+//
+//  Created by Marnie Reid on 5/24/20.
+//  Copyright © 2020 Marnie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
